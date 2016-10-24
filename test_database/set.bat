92e14c3221ede761f8084a58440271b61e16bf40
ElderMayday <nerdy308deez44>
2016-10-24 20:01:54 +0200

    Put test data to files

--- test_database/set.bat
+++ test_database/set.bat
@@ -4,38 +4,9 @@ curl -X PUT http://127.0.0.1:5984/social
 
 :: Users of the social network ---------------------------------------------------------------------------------------
 
-curl -X PUT http://127.0.0.1:5984/social/cff457c34484830b569a999a27014134 -d ^
-"{^
-	\"type\":\"user\",^
-	\"username\":\"aldar\",^
-	\"first_name\":\"Aldar\",^
-	\"second_name\":\"Saranov\",^
-	\"password_hash\":\"827ccb0eea8a706c4c34a16891f84e7b\",^
-	\"date_of_birth\":\"10.05.1995\",^
-	\"description\":\"Here is description about this user\"^
-}"
-
-curl -X PUT http://127.0.0.1:5984/social/cff457c34484830b569a999a27016314 -d ^
-"{^
-	\"type\":\"user\",^
-	\"username\":\"najim\",^
-	\"first_name\":\"Najim\",^
-	\"second_name\":\"Essakali\",^
-	\"password_hash\":\"827ccb0eea8a706c4c34a16891f84e7b\",^
-	\"date_of_birth\":\"01.01.1995\",^
-	\"description\":\"Here is description about this user\"^
-}"
-
-curl -X PUT http://127.0.0.1:5984/social/a8af1fde90e8f71681da7ce37300242f -d ^
-"{^
-	\"type\":\"user\",^
-	\"username\":\"john \",^
-	\"first_name\":\"John\",^
-	\"second_name\":\"Smith\",^
-	\"password_hash\":\"827ccb0eea8a706c4c34a16891f84e7b\",^
-	\"date_of_birth\":\"01.01.1995\",^
-	\"description\":\"Here is description about this user\"^
-}"
+curl -X PUT http://127.0.0.1:5984/social/cff457c34484830b569a999a27014134 -d @user_1.txt
+curl -X PUT http://127.0.0.1:5984/social/cff457c34484830b569a999a27016314 -d @user_2.txt
+curl -X PUT http://127.0.0.1:5984/social/a8af1fde90e8f71681da7ce37300242f -d @user_3.txt
 
 :: Design documents ----------------------------------------------------------------------------------------------------
 
